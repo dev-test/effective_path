ae7a6b16f2a02f896d06e35d2a85ec7d30678662
Jerome <jerome.paul@gmail.com>
2011-09-08 18:54:38 +1000

    the second base test

--- effective_path_test.rb
+++ effective_path_test.rb
@@ -15,4 +15,10 @@ describe EffectivePath do
     end
   end 
 
+  context "when the string does not contain operators for the current(.) or parent(..) directories" do
+    it "should return the string unchanged" do
+      @effective_path.path_for("c:/dir1/dir2/").should == "c:/dir1/dir2/"
+    end 
+  end
+  
 end
